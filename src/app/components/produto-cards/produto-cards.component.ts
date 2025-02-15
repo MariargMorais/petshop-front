9d08fbd8b06cf18521357e57e6d4acaf0323790a
Juliana Ribeiro <julianarpaz@gmail.com>
2025-02-15 18:53:18 -0300

    add: fluxo de login, cadastro e agendamento

--- src/app/components/produto-cards/produto-cards.component.ts
+++ src/app/components/produto-cards/produto-cards.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produto-cards',
   standalone: true,
-  imports: [],
   templateUrl: './produto-cards.component.html',
   styleUrl: './produto-cards.component.css',
+  imports: [CommonModule],
 })
 export class ProdutoCardsComponent {
-  @Input() product!: { name: string; price: number };
+  @Input() product!: { nome: string; descricao: string; preco: number; imagem: string };
 }
